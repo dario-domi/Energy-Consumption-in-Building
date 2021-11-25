8d8f433fb0c7e11d225378487cb8be9a2503091a
dario-domi <dario.domingo@durham.ac.uk>
2021-11-25 16:16:52 +0000

    Pictures/Simulation_hist/Plot_Output_Histograms.R now produces 3x4 picture in publication style

--- Pictures/Simulation_histograms/Batch_2_Only/Plot_Output_Histograms.R
+++ Pictures/Simulation_histograms/Batch_2_Only/Plot_Output_Histograms.R
@@ -1,140 +1,202 @@
 #############################################################################
 #
-# For both gas and electricity, and for each of the 12 months, this script 
-# produces histograms of the 1000 simulated consumptions. A vertical line is
-# added to denote the value of the observed consumption.
+# For both gas and electricity, a 3x4 plot with histograms of the 1000 simulated
+# consumptions for each month is produced. A vertical line is added at the 
+# observed consumption.
+# Moreover, 12 plots of the single histograms are produced.
 #
-# This script and the pictures produced (in pdf) can be found at:
+# This script and the pictures produced (in png/pdf) can be found at:
 # 'UQ_Energy_Building/Pictures/Simulation_Histograms/Batch_2_Only'
 #
 #############################################################################
 
+#########################################################################
+#  SET PRELIMINARY VARIABLES AND FUNCTIONS 
+#########################################################################
 
-# SET FOLDER
+
+## Set folder
 setwd('/Users/Durham/Desktop/PostDoc/Projects/UQ_Energy_Building/Pictures/')
 
-## COLOUR OF HISTOGRAM BARS AND VERTICAL LINE (OBSERVED DATA)
+## Colour of histogram bars and vertical line (observed data)
 col_hist <- "#00A600FF"
-col_line <- "#4C00FFFF" # blue
-col_line <- "#FF0000FF" # red-orange
 col_line <- "#FF5500FF" # orange
 
+## CUSTOMISED FUNCTION PLOTTING HISTOGRAM + VERTICAL LINE
+# sim: vector of simulated values (used for histogram)
+# obs: one value for the observation (vertical line added at that value)
+# fac: factor for rescaling the (min, max) range of c(sim, obs)
+# l_wdt: line width for added vertical line.
+# If length(sim)=1, then a vertical line rather than a histogram is produced
 
-#######################################################################
-## ACTUAL PLOTS, GAS AND ELECTRICITY (histogram plus vertical bar)
+myplot <- function(sim, obs, fac, l_wdt){
+  
+  # Find range including simulation outputs and observation, 
+  # and rescale it by 'fac' times around its center
+  rg <- range(c(sim, obs)) 
+  shift <- (fac-1)*( (rg[1]+rg[2])/2 )    
+  rg <- fac*rg - shift                
+  
+  # If only one sim value, plot a vertical line for it. Otherwise histogram
+  if (length(sim)==1){
+    hist(0, xlim = rg, main="", xlab="", ylab="")  # empty histogram 
+    abline(v=sim, col=col_hist, lwd=l_wdt, lty=2)  # add vertical line at simulated value
+  }
+  else{
+    hist(sim, breaks=nbin[month], xlim=rg, col=col_hist, 
+         main="", xlab="", ylab="")
+  }
+  
+  # Add vertical line corresponding to observation
+  abline(v=obs, col=col_line, lwd= l_wdt, lty=2)
+}
 
-## GAS ##
+
+#########################################################################
+####                            GAS PLOTS
+#########################################################################
 
 # Load gas data
 load('../RCode/RData/Inputs/Simulated_and_Observed_Gas.RData')
 
 ## Number of bins for each plot (by month)
 #              J  F  M  A  M  J  J  A  S  O  N  D
-nbin.gas <-  c(15,16,15,15,14, 8, 9,15,15,15,15,15)
+nbin <-  c(15,16,15,15,14, 8, 9,15,15,15,15,15)
+
+
+#################################################
+# 3x4 PLOT WITH GAS HISTOGRAMS FOR ALL MONTHS
+
+file.name <- 'Simulation_histograms/Batch_2_Only/Gas_Runs/All_months_gas.png'
+ln = 1.3                     # distance between title and plot box
+
+png(file.name, width = 8, height = 6, unit="in", res=288)
+par(mfrow=c(3,4), 
+    mai=c(0.5, 0.6, 0.3, 0), # for each subplot: bott, left, top, right margins
+    mgp = c(2.8,1,0)         # mgp[1] = dist label-axis; mgp[2] = dist numbers-axis; 
+                             # mgp[3] = dist axis line from plot box. Default: 3,1,0
+)
 
 for (month in 1:12){
-  x <- Gas.Sim[, month]
+  sim <- Gas.Sim[, month]
   obs <- Gas.Obs[month]
+  fac <- 1.1               # Factor rescaling range of histograms's x-axis
+
+  # Plot
+  myplot(sim, obs, fac, 3.5)                            # Histogram + vertical line 
+  title( paste(month.name[month], 'Gas'), line = ln)    # Title
   
-  # FIND SUITABLE RANGE INCLUDING BOTH SIMULATION OUTPUTS AND OBSERVED DATA
-  rg <- range(c(x, obs)) 
-  fac <- 1.1
-  # The following lines widen the interval rg of a factor fac, around its centre
-  shift <- (fac-1)*( (rg[1]+rg[2])/2 )
-  rg <- fac*rg - shift
+  # If one of the left-most plots, add y-label
+  if (month%%4 == 1){
+    title(ylab = "Frequency")
+  }
+  # If last row, add x-label
+  if (month>8.5){
+    title(xlab = "Gas consumption [kWh]")
+  }
+}
+dev.off()
+
+
+##########################################
+# PLOTS OF SINGLE HISTOGRAMS (GAS)
+
+for (month in 1:12){
+  sim <- Gas.Sim[, month]
+  obs <- Gas.Obs[month]
+  fac <- 1.1               # Factor rescaling range of histograms's x-axis
   
-  # Title string of plot
-  th <- paste(month.names[month], 'Gas consumption')
-  
-  file.name <- paste('Simulation_histograms/Batch_2_Only/Gas_Runs/', 
+  file.name <- paste('Simulation_histograms/Batch_2_Only/Gas_Runs/Single_months/', 
                      month.names[month], "_Gas.pdf", sep = "")
   pdf(file.name)
-  par(cex=1.6, lwd=1.5)
   
-  # Plot the histogram of 1000 simulated consuptions
-  hist(x, main=th, breaks=nbin.gas[month], xlim=rg, col=col_hist, 
-       xlab="Gas consumption [kWh]")
-  
-  # Add vertical line corresponding to observation
-  abline(v=obs, col=col_line, lwd=6.5, lty=2)
-  
+  # Plot 
+  par(cex=1.6, lwd=1.5)                 # Graphical parameters
+  myplot(sim, obs, fac, 6.5)            # Histogram + vertical line 
+  th <- paste(month.names[month], 'Gas consumption')
+  title(main = th, xlab="Gas consumption [kWh]", ylab = "Frequency")
+
   dev.off()
 }
 
 
-## ELECTRICITY ##
 
-# Load gas data
+#########################################################################
+##########               ELECTRICITY PLOTS          
+########################################################################
+
+
+# Load electricity data
 load('../RCode/RData/Inputs/Simulated_and_Observed_Elec.RData')
 
 ## Number of bins for each plot (by month)
 #               J  F  M  A  M   J   J    A  S  O  N  D
-nbin.elec <-  c(15,16,15,15,14, NA, NA, NA,15,15,15,15)
+nbin <-  c(15,16,15,15,14, NA, NA, NA,15,15,15,15)
 
-# The code for the plots of June, July and August is done separately, since each 
-# of these shows no variation at all in the simulated electricity consumption
+# Summer months show no variation at all in the simulated electricity consumption,
+# so future plots will be different for summer and non_summer months.
+summer <- c(6,7,8)  # June, July, August
 
-# Non-summer months
-for (month in c(1:5, 9:12)){
-  x <- Elec.Sim[, month]
+
+#################################################
+# 3x4 PLOT WITH ELEC HISTOGRAMS FOR ALL MONTHS
+
+file.name <- 'Simulation_histograms/Batch_2_Only/Elec_Runs/All_months_elec.png'
+
+png(file.name, width = 8, height = 6, unit="in", res=288)
+par(mfrow=c(3,4), 
+    mai=c(0.5, 0.6, 0.3, 0), # for each subplot: bott, left, top, right margins
+    mgp = c(2.8,1,0)         # mgp[1] = dist label-axis; mgp[2] = dist numbers-axis;
+                             # mgp[3] = dist axis line from plot box. Default: 3,1,0
+)
+
+for (month in 1:12){
+  
+  # Take just first or all simulated values according to summer/non_summer
+  if (month %in% summer) sim <- Elec.Sim[1, month]
+  else sim <- Elec.Sim[, month]
+  
   obs <- Elec.Obs[month]
+  # Factor rescaling range of histograms's x-axis
+  fac <- ifelse(month %in% summer, 1.2, 1.1)    # = 1.2 for summer, 1.1 otherwise
+
+  # Plot
+  myplot(sim, obs, fac, 3.5)                            # Histogram + vertical line 
+  title( paste(month.name[month], 'Elec'), line = ln)    # Title
   
-  # FIND SUITABLE RANGE INCLUDING BOTH SIMULATION OUTPUTS AND OBSERVED DATA
-  rg <- range(c(x, obs)) 
-  fac <- 1.1
-  # The following lines widen the interval rg of a factor fac, around its centre
-  shift <- (fac-1)*( (rg[1]+rg[2])/2 )
-  rg <- fac*rg - shift
-  
-  # Title string of plot
-  th <- paste(month.names[month], 'Elec consumption')
-  
-  file.name <- paste('Simulation_histograms/Batch_2_Only/Elec_Runs/', 
-                     month.names[month], "_Elec.pdf", sep = "")
-  pdf(file.name)
-  par(cex=1.6, lwd=1.5)
-  
-  # Plot the histogram of 1000 simulated consuptions
-  hist(x, main=th, breaks=nbin.elec[month], xlim=rg, col=col_hist, 
-       xlab="Electricity consumption [kWh]")
-  
-  # Add vertical line corresponding to observation
-  abline(v=obs, col=col_line, lwd=6.5, lty=2)
-  
-  dev.off()
+  # If one of the left-most plots, add y-label
+  if (month%%4 == 1){
+    title(ylab = "Frequency")
+  }
+  # If last row, add x-label
+  if (month>8.5){
+    title(xlab = "Elec consumption [kWh]")
+  }
 }
+dev.off()
 
 
-# The 3 summer months
-# Non-summer months
-for (month in 6:8){
-  sim <- Elec.Sim[1, month]
+################################################
+# PLOTS OF SINGLE HISTOGRAMS (ELECTRICITY)
+
+for (month in 1:12){
+  # Take just first or all simulated values according to summer/non_summer
+  if (month %in% summer) sim <- Elec.Sim[1, month]
+  else sim <- Elec.Sim[, month]
+  
   obs <- Elec.Obs[month]
+  # Factor rescaling range of histograms's x-axis
+  fac <- ifelse(month %in% summer, 1.2, 1.1)    # = 1.2 for summer, 1.1 otherwise
   
-  # FIND SUITABLE RANGE INCLUDING BOTH SIMULATION OUTPUTS AND OBSERVED DATA
-  rg <- range(c(sim, obs)) 
-  fac <- 1.2
-  # The following lines widen the interval rg of a factor fac, around its centre
-  shift <- (fac-1)*( (rg[1]+rg[2])/2 )
-  rg <- fac*rg - shift
-  
-  # Title string of plot
-  th <- paste(month.names[month], 'Elec consumption')
-  
-  file.name <- paste('Simulation_histograms/Batch_2_Only/Elec_Runs/', 
+  file.name <- paste('Simulation_histograms/Batch_2_Only/Elec_Runs/Single_months/', 
                      month.names[month], "_Elec.pdf", sep = "")
   pdf(file.name)
-  par(cex=1.6, lwd=1.5)
   
-  # Plot the histogram of 1000 simulated consuptions
-  hist(2*max(sim, obs), main=th, xlim=rg,
-       xlab="Electricity consumption [kWh]")
-  
-  # Add vertical line corresponding to simulated value
-  abline(v=sim, col=col_hist, lwd=6.5, lty=2)
-  
-  # Add vertical line corresponding to observation
-  abline(v=obs, col=col_line, lwd=6.5, lty=2)
+  # Plot 
+  par(cex=1.6, lwd=1.5)                 # Graphical parameters
+  myplot(sim, obs, fac, 6.5)            # Histogram + vertical line 
+  th <- paste(month.names[month], 'Elec consumption')
+  title(main = th, xlab="Electricity consumption [kWh]", ylab = "Frequency")
   
   dev.off()
 }
